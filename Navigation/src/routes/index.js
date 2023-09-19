ac7d61677a887d6d1aa5f0b56dfe6e760411b258
nicolas_nagano <nicolasjap@hotmail.com>
2023-09-19 14:52:49 -0300

    finalizado navegation Tabs

--- Navigation/src/routes/index.js
+++ Navigation/src/routes/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
-import Stack from './Stack'
-
+// import Stack from './Stack'
+import Tabs from './Tabs'
 
 export default props => (
   <SafeAreaView style={{flex: 1}}>
     <NavigationContainer>
-      <Stack />
+      {/* <Stack /> */}
+      <Tabs />
     </NavigationContainer>
   </SafeAreaView>
 )
