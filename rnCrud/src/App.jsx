83ab7047d241cf6889d1c18764092a6b8b9de975
Nicolas Nagano <nicolasjap@hotmail.com>
2023-09-21 15:05:10 -0300

    criado app.jsx

--- rnCrud/src/App.jsx
+++ rnCrud/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
+import UserList from './views/UserList'
+import UserForm from './views/UserForm'
+import { Button } from 'react-native-elements'
 
 const Stack = createStackNavigator()
 
@@ -8,9 +11,42 @@ export default props => {
 
     return (
         <NavigationContainer>
-            <Stack.Screen>
-                
-            </Stack.Screen>
+            <Stack.Navigator initialRouteName='UserList'
+            screenOptions={screenOptions}
+            >
+                <Stack.Screen 
+                    name='UserList'
+                    component={UserList}
+                    options={() => { 
+                    return{
+                        title: "Lista de Usuarios",
+                        headerRight: () => {
+                            <Button 
+                                type='clear'
+                                icon={<Icon name='add' size={25} color='white'/>}
+                            />
+                        }
+                        }
+                    }}  
+                />
+                <Stack.Screen 
+                    name='UserForm'
+                    component={UserForm}
+                    options={{
+                        title: 'Formulario de Usuarios'
+                    }}
+                />
+            </Stack.Navigator>
         </NavigationContainer>
     )
+}
+
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: '#219C90'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+        fontWeight: 'bold'
+    }
 }
