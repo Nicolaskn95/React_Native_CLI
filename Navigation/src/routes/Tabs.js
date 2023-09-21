f2c1593831f18113c0f450f7912182238172529d
nicolas_nagano <nicolasjap@hotmail.com>
2023-09-21 13:13:20 -0300

    initial_project

--- Navigation/src/routes/Tabs.js
+++ Navigation/src/routes/Tabs.js
@@ -5,15 +5,39 @@ import TelaB from '../views/TelaB'
 import TelaC from '../views/TelaC'
 import PassoStack from '../components/PassoStack'
 import TelaD from '../views/TelaD'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator()
 
 export default props => (
-  <Tab.Navigator screenOptions={{
-    activeTintColor: 'pink',
-    inactiveTintColor: 'blue',
-    labelStyle: {fontSize: 30 }
-    }} initialRouteName='TelaB'>
+  <Tab.Navigator         screenOptions={({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
+
+      switch (route.name) {
+  case 'TelaA':
+    iconName = focused ? 'ios-home' : 'ios-home-outline';
+    break;
+  case 'TelaB':
+    iconName = focused ? 'ios-list' : 'ios-list-outline';
+    break;
+  case 'TelaC':
+    iconName = focused ? 'ios-person' : 'ios-person-outline';
+    break;
+  case 'TelaD':
+    iconName = focused ? 'ios-settings' : 'ios-settings-outline';
+    break;
+  default:
+    iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+    break;
+}
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+  })}
+    tabBarOptions={{
+      activeTintColor: 'red'
+    }}
+     initialRouteName='TelaB'>
     <Tab.Screen name="TelaA" component={TelaA}/>
     <Tab.Screen name="TelaB" component={TelaB}/>
     <Tab.Screen name="TelaC" component={TelaC}/>
